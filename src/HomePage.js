efbf8918d986e60f16548203443251d376706a04
Hallie <70453087+ha1lie@users.noreply.github.com>
2021-09-16 20:10:49 -0400

    Setup for API changes
    But the cool thing is the hacker stuff to make sure it still works in the meantime.... (insert devil horn emoji)

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
-import { Box, Text, Anchor, Paragraph, ResponsiveContext, Button } from 'grommet';
+import { Box, Text, Anchor, Spinner, ResponsiveContext, Button } from 'grommet';
 import { isMacOs } from 'react-device-detect';
-import { DownloadOption, PhoneVertical } from 'grommet-icons';
+import { DownloadOption, PhoneVertical, Close } from 'grommet-icons';
 
 class HomePage extends Component {
+
+  async fetchOnline() {
+    let maintenence = true
+    await fetch("https://api.halz.dev/isMaintenence", {
+      method: 'GET'
+    }).then(function(res) {
+      return res.text()
+    }).then(function(text) {
+      maintenence = text == "NO" //FIX ME
+    })
+    this.setState({isDown: maintenence})
+  }
+
+  async fetchCount() {
+    let tmpCount = ""
+    await fetch("https://api.halz.dev/linkly/count", {
+      method: 'GET'
+    }).then(function(res) {
+      return res.text()
+    }).then(function(text) {
+      tmpCount = text
+    })
+    console.log("THIS IS TMP COUNT:")
+    console.log(tmpCount)
+    this.setState({count: tmpCount})
+  }
+
+  async downloadLinkly() {
+    this.setState({loadingDownload: true})
+    await fetch("https://api.halz.dev/linkly/download", {
+      method: 'GET'
+    }).then(function(res) {
+      return res.text()
+    }).then(function(url) {
+      window.open(url)
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isDown: null,
+      count: "",
+      loadingDownload: false
+    }
+    this.fetchOnline()
+    this.fetchCount()
+  }
+
   render() {
     return (
       <ResponsiveContext.Consumer>
@@ -11,25 +60,67 @@ class HomePage extends Component {
           <Box flex='grow' align='center' justify='center' pad='medium' >
             <Text color='appBarBackground' weight='700' size={ (size == 'medium' || size == 'large' || size == 'xlarge' ) ? '850%' : '500%'} margin={{left: '20pt', bottom: 'none' }} >Linkly</Text>
             <Text color='appBarBackground' textAlign='center' weight='600' size='200%'>Simple and Convenient Menu Bar Link Manager</Text>
-            { isMacOs ? (
-              <Button fill='horizontal' href='https://api.halz.dev/files/Linkly.zip' target='_blank'>
-                <Box background='#5e5ce6' direction='column' align='center' margin='large' pad="medium" round='medium'>
-                  <Box align='center' direction='row' pad='xsmall' gap='small'>
-                    <Text weight='bold' size='150%'>Download Now</Text>
-                    <DownloadOption />
+            { }
+            {
+              this.state.loadingDownload ? (
+                <Box align='center' fill='horizontal' background='#5e5ce6' pad='medium' round='medium'>
+                  <Box>
+                    <Box direction='row' justify='between' align='center' margin='small'>
+                      <Box direction='row' align='center' gap='medium'>
+                        <Spinner color='white' size='medium' />
+                        <Text weight='700' size='125%' >Downloading...</Text>
+                      </Box>
+                      <Close onClick={ () => { this.setState({ loadingDownload: false }) } } />
+                    </Box>
+                    <Box>
+                      <Text weight='600' size='120%'>To install Linkly:</Text>
+                      <Text>1. Open zip file</Text>
+                      <Text>2. Drag Linkly to your Applications folder</Text>
+                      <Text>3. Open Linkly once, and never worry about it again!</Text>
+                    </Box>
                   </Box>
-                  <Text size='70%'>Supports MacOS 11.0 or Higher</Text>
                 </Box>
-              </Button>
+              ) : (
+                this.state.isDown === null ? (
+                  <Spinner color='#5e5ce6' size='medium' />
+                ) : ((this.state.isDown) ? (
+                  <Box fill='horizontal' margin='large' align='center' background='#5e5ce6' round='medium' pad='medium'>
+                    <Box>
+                      <Text weight='700' size='150%'>I'm sorry,</Text>
+                      <Text>Our backend server is offline for maintenence right now, please come back to download linkly soon!</Text>
+                    </Box>
+                  </Box>
+                ) : (
+                  isMacOs ? (
+                    <Button onClick={ () => { this.downloadLinkly() } } fill='horizontal'>
+                      <Box background='#5e5ce6' direction='column' align='center' margin='large' pad="medium" round='medium'>
+                        <Box align='center' direction='row' pad='xsmall' gap='small'>
+                          <Text weight='bold' size='150%'>Download Now</Text>
+                          <DownloadOption />
+                        </Box>
+                        <Text size='70%'>Supports MacOS 11.0 or Higher</Text>
+                      </Box>
+                    </Button>
+                  ) : (
+                    <Box background='#5e5ce6' direction='column' align='center' margin='large' pad="medium" round='medium'>
+                      <Box align='center' direction='row' pad='xsmall' gap='small'>
+                        <Text weight='bold' size='150%'>I'm sorry, you can't download Linkly</Text>
+                        <PhoneVertical />
+                      </Box>
+                      <Text size='70%'>Visit page on a Mac device to download Linkly</Text>
+                      <Anchor href="https://api.halz.dev/files/Linkly.zip" size='50%' color='white' style={{ textDecoration: 'underline'}}>Seeing this in error? Download anyway</Anchor>
+                    </Box>
+                  )
+                ))
+              )
+            }
+            { this.state.count != "" ? (
+              <Box pad='small'>
+                <Text>Join {this.state.count} other people using Linkly</Text>
+              </Box>
             ) : (
-              <Box background='#5e5ce6' direction='column' align='center' margin='large' pad="medium" round='medium'>
-                  <Box align='center' direction='row' pad='xsmall' gap='small'>
-                    <Text weight='bold' size='150%'>I'm sorry, you can't download Linkly</Text>
-                    <PhoneVertical />
-                  </Box>
-                  <Text size='70%'>Visit page on a Mac device to download Linkly</Text>
-                  <Anchor href="https://api.halz.dev/files/Linkly.zip" size='50%' color='white' style={{ textDecoration: 'underline'}}>Seeing this in error? Download anyway</Anchor>
-                </Box>
+              <Box>
+              </Box>
             )}
             <Text color='appBarBackground' textAlign='center' weight='600' size='200%'>About Linkly</Text>
             <Box align='center' gap='medium'>
@@ -45,7 +136,6 @@ class HomePage extends Component {
               <Text>You bet! The <a color="#5e5ce6" href="https://github.com/ha1lie/Linkly" target="_blank" style={{ textDecoration: 'none'}}>App</a> and the <a color="#5e5ce6" href="https://github.com/ha1lie/linkly-halz-dev" target="_blank" style={{ textDecoration: 'none'}}>Website</a> are both opensource on GitHub! Give them some stars!</Text>
               <Text weight='600' size='120%'>Questions?</Text>
               <Text>Contact Hallie <a color="#5e5ce6" href="/contact" target="_self" style={{ textDecoration: 'none'}}>HERE</a></Text>
-  
             </Box>
           </Box>
         )}
